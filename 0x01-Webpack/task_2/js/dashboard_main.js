c3782b00f836e3611a8993504ea3b852b9f9915b
Mxn-ptr <maxence0@hotmail.fr>
2023-05-04 18:34:29 +0200

    Add task 2

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import '../css/main.css';
+import $ from 'jquery';
+import _ from 'lodash';
+
+let count = 0;
+
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+const updateCounter = () => {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+  }
+
+$(() => {
+  $('button').on('click', _.debounce(updateCounter, 500));
+})
+
