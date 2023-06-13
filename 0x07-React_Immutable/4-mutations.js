727da25c253335a5c6c2a5310dbbdfd5df2d2ad8
Mxn-ptr <maxence0@hotmail.fr>
2023-06-13 09:13:26 +0200

    Fix task 4

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-export const map1 = Map({
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,6 +9,6 @@ export const map1 = Map({
   6: 'Lucas',
 });
 
-export const map2 = map1.withMutations((map) => {
-  map.set('2', 'Benjamin').set('4', 'Oliver');
+export const map2 = map.withMutations((e) => {
+  e.set('2', 'Benjamin').set('4', 'Oliver');
 });
